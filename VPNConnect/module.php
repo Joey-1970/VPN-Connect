bb6ef474f8fee4df5d3c1ffd7a81b3d1ad649a37
Joey-1970 <paeper@horburg.de>
2022-02-06 20:33:14 +0100

    Update module.php

--- VPNConnect/module.php
+++ VPNConnect/module.php
@@ -11,14 +11,28 @@
             	parent::Create();
             	$this->RegisterPropertyBoolean("Open", false);
 		$this->RegisterPropertyString("IPAddress", "127.0.0.1");
-		$this->RegisterPropertyInteger("Port", 6600);
+		$this->RegisterPropertyInteger("MaxWaitTime", 100);
+		$this->RegisterPropertyInteger("Tries", 5);
 		
-		$this->RegisterTimer("ConnectionTest", 0, 'VPNConnect_ConnectionTest($_IPS["TARGET"]);');
+		$this->RegisterTimer("ConnectionTest", 0, 'VPNConnect_GetDataUpdate($_IPS["TARGET"]);');
 		
 		// Profile anlegen
+		$this->RegisterProfileInteger("VPNConnect.State", "Information", "", "", 0, 3, 1);
+		IPS_SetVariableProfileAssociation("VPNConnect.State", 0, "Unbekannt", "Information", -1);
+		IPS_SetVariableProfileAssociation("VPNConnect.State", 1, "Offline", "Close", 0xFF0000);
+		IPS_SetVariableProfileAssociation("VPNConnect.State", 2, "Störung", "Alert", 0xFFFF00);
+		IPS_SetVariableProfileAssociation("VPNConnect.State", 3, "Online", "Network", 0x00FF00);
+		
+		$this->RegisterProfileFloat("VPNConnect.ms", "Clock", "", " ms", 0, 1000, 0.001, 3);
 		
 		
-		
+		// Status-Variablen anlegen
+		$this->RegisterVariableInteger("LastUpdate", "Letztes Update", "~UnixTimestamp", 10);
+	
+		$this->RegisterVariableInteger("SuccessRate", "Erfolgsqoute", "~Intensity.100", 60);
+		$this->RegisterVariableFloat("MinDuration", "Minimale Dauer", "IPS2LAN.ms", 70);
+		$this->RegisterVariableFloat("AvgDuration", "Durchschnittliche Dauer", "IPS2LAN.ms", 80);
+		$this->RegisterVariableFloat("MaxDuration", "Maximale Dauer", "IPS2LAN.ms", 90);
 
         }
        	
@@ -32,8 +46,10 @@
 		
 		$arrayElements = array(); 
 		$arrayElements[] = array("name" => "Open", "type" => "CheckBox",  "caption" => "Aktiv"); 
-		$arrayElements[] = array("type" => "ValidationTextBox", "name" => "IPAddress", "caption" => "IP");
-		$arrayElements[] = array("type" => "NumberSpinner", "name" => "Port", "caption" => "Port (1 - 65535)", "minimum" => 1, "maximum" => 65535);
+		$arrayElements[] = array("type" => "ValidationTextBox", "name" => "IPAddress", "caption" => "IP die zum Test im VPN-Zielnetz angepingt werden soll");
+		$arrayElements[] = array("type" => "NumberSpinner", "name" => "MaxWaitTime", "caption" => "Maximale Wartezeit Ping (50 - 1000)", "minimum" => 50, "maximum" => 1000, "suffix" => "ms");
+		$arrayElements[] = array("type" => "NumberSpinner", "name" => "Tries", "caption" => "Versuche (2 - 15)", "minimum" => 2, "maximum" => 15, "suffix" => "Anzahl");
+
 		
 		
 				
@@ -86,47 +102,135 @@
 	    	}
 	}
 	    
-	
-	
-	
-	    
-	
-	    
-	public function ConnectionTest()
+	public function GetDataUpdate()
 	{
-	      	$result = false;
-		return $result;
 		
+		$Result = unserialize($this->Multiple_Ping());
+		$Ping = $Result["Ping"];
+		$SuccessRate = $Result["SuccessRate"];
+		$MinDuration = $Result["MinDuration"];
+		$AvgDuration = $Result["AvgDuration"];
+		$MaxDuration = $Result["MaxDuration"];
 		
-		$IPAddress = $this->ReadPropertyString("IPAddress");
-		$Port = $this->ReadPropertyInteger("Port");
-	      	If (Sys_Ping($IPAddress, 300)) {
-			$status = @fsockopen($IPAddress, $Port, $errno, $errstr, 10);
-			if (!$status) {
-				$this->SendDebug("ConnectionTest", "Port ".$Port." ist geschlossen!", 0);
-				IPS_LogMessage("MusicPlayerDaemon","Port ".$Port." ist geschlossen!");
-				If ($this->GetStatus() <> 202) {
-					$this->SetStatus(202);
+		/*
+	
+		If ($Ping <> GetValueInteger($this->GetIDForIdent("State"))) {
+			$SentDisorder = $this->ReadPropertyBoolean("SentDisorder");
+			If ($Ping == 1) { // offline
+				$this->Notification($this->ReadPropertyString("TextDown"));
+			}
+			elseif ($Ping == 2) { // gestört
+				If ($SentDisorder == true) {
+					$this->Notification($this->ReadPropertyString("TextDisorder"));
 				}
 			}
-		      	else {
-				$result = true;
-				If ($this->GetStatus() <> 102) {
-					$this->SetStatus(102);
+			If ($Ping == 3) { // online
+				If ($SentDisorder == true) {
+					$this->Notification($this->ReadPropertyString("TextUp"));
+				}
+				elseif (($SentDisorder == false) AND (GetValueInteger($this->GetIDForIdent("State")) <> 2)) {
+					$this->Notification($this->ReadPropertyString("TextUp"));
 				}
 			}
+			SetValueInteger($this->GetIDForIdent("State"), $Ping);
 		}
-		else {
-			$this->SendDebug("ConnectionTest", "IP ".$IPAddress." reagiert nicht!", 0);
-			IPS_LogMessage("MusicPlayerDaemon","IP ".$IPAddress." reagiert nicht!");
-			If ($this->GetStatus() <> 202) {
-				$this->SetStatus(202);
-			}
+		*/
+		If ($SuccessRate <> $this-GetValue("SuccessRate")) {
+			$this-SetValue("SuccessRate", $SuccessRate);
 		}
-	return $result;
+		If ($MinDuration <> $this-GetValue("MinDuration")) {
+			$this-SetValue("MinDuration", $MinDuration);
+		}
+		If ($AvgDuration <> $this-GetValue("AvgDuration")) {
+			$this-SetValue("AvgDuration", $AvgDuration);
+		}
+		If ($MaxDuration <> $this-GetValue("MaxDuration")) {
+			$this-SetValue("MaxDuration", $MaxDuration);
+		}
+		$this-SetValue("LastUpdate", time() );
+		
 	}
 	
+	private function Multiple_Ping()
+	{
+    		$this->SendDebug("Multiple_Ping", "Ausfuehrung", 0);
+		$IP = $this->ReadPropertyString("IP");
+		$MaxWaitTime = $this->ReadPropertyInteger("MaxWaitTime");
+		$MaxWaitTime = min(1000, max(50, $MaxWaitTime));
+		$Tries = $this->ReadPropertyInteger("Tries");
+		$Tries = min(15, max(2, $Tries));
+		$Result = array();
+		$Ping = array();
+		$Duration = array();
+		
+		for ($i = 0; $i < $Tries; $i++) {
+			$Start = microtime(true);
+			$Response = Sys_Ping($IP, $MaxWaitTime); 
+			$Duration[] = microtime(true) - $Start;
+			$Ping[] = $Response;
+			
+		}
+		// Ping-Werte berechnen
+		$MinDuration = round(min($Duration) * 1000, 2);
+		$AvgDuration = round((array_sum($Duration)/count($Duration)) * 1000, 2);
+		$MaxDuration = round(max($Duration) * 1000, 2);
+		// Erfolg auswerten
+		$SuccessRate = Round((array_sum($Ping)/count($Ping)) * 100, 2);
+		$this->SendDebug("Multiple_Ping", "Min: ".$MinDuration."ms, Durchschnitt: ".$AvgDuration."ms, Max: ".$MaxDuration."ms, Erfolg: ".$SuccessRate."%"." Versuche: ".(count($Ping)), 0);
+		If ($SuccessRate == 100) {
+			$Result["Ping"] = 3;
+		}
+		elseif ($SuccessRate == 0) {
+			$Result["Ping"] = 1;
+		}
+		else {
+			$Result["Ping"] = 2;
+		}
+		$Result["SuccessRate"] = $SuccessRate;
+		$Result["MinDuration"] = $MinDuration;
+		$Result["AvgDuration"] = $AvgDuration;
+		$Result["MaxDuration"] = $MaxDuration;
+	return serialize($Result);
+	}   
+	    
 	
+	    
+	
+	
+	private function RegisterProfileInteger($Name, $Icon, $Prefix, $Suffix, $MinValue, $MaxValue, $StepSize)
+	{
+	        if (!IPS_VariableProfileExists($Name))
+	        {
+	            IPS_CreateVariableProfile($Name, 1);
+	        }
+	        else
+	        {
+	            $profile = IPS_GetVariableProfile($Name);
+	            if ($profile['ProfileType'] != 1)
+	                throw new Exception("Variable profile type does not match for profile " . $Name);
+	        }
+	        IPS_SetVariableProfileIcon($Name, $Icon);
+	        IPS_SetVariableProfileText($Name, $Prefix, $Suffix);
+	        IPS_SetVariableProfileValues($Name, $MinValue, $MaxValue, $StepSize);        
+	}    
+	    
+	private function RegisterProfileFloat($Name, $Icon, $Prefix, $Suffix, $MinValue, $MaxValue, $StepSize, $Digits)
+	{
+	        if (!IPS_VariableProfileExists($Name))
+	        {
+	            IPS_CreateVariableProfile($Name, 2);
+	        }
+	        else
+	        {
+	            $profile = IPS_GetVariableProfile($Name);
+	            if ($profile['ProfileType'] != 2)
+	                throw new Exception("Variable profile type does not match for profile " . $Name);
+	        }
+	        IPS_SetVariableProfileIcon($Name, $Icon);
+	        IPS_SetVariableProfileText($Name, $Prefix, $Suffix);
+	        IPS_SetVariableProfileValues($Name, $MinValue, $MaxValue, $StepSize);
+	        IPS_SetVariableProfileDigits($Name, $Digits);
+	}
 
 
 }
